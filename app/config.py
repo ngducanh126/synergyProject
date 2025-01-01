f557e3e11c2a2cbee92cb7e92a318e4a77b91dae
anh <ngducanh126@gmail.com>
2024-12-31 22:14:01 -0500

    code for deployment

--- app/config.py
+++ app/config.py
@@ -2,10 +2,17 @@ import os
 
 class Config:
     SECRET_KEY = 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:hanoihue@localhost:5432/synergy'
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///local.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'your-jwt-secret-key'
     BASE_URL = 'http://127.0.0.1:5000'  # Base URL for serving uploaded files
-    UPLOAD_FOLDER_COLLABORATIONS = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'uploads/collaborations')
-    UPLOAD_FOLDER_PROFILE = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'uploads/users')
+    UPLOAD_FOLDER_COLLABORATIONS = os.getenv(
+        "UPLOAD_FOLDER_COLLABORATIONS",
+        os.path.join(os.getcwd(), 'uploads/collaborations'),
+    )
+    UPLOAD_FOLDER_PROFILE = os.getenv(
+        "UPLOAD_FOLDER_PROFILE",
+        os.path.join(os.getcwd(), 'uploads/users'),
+    )
+
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
