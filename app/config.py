11264866df8ac574062425f5882b97dfa29e8a23
anh <ngducanh126@gmail.com>
2025-01-01 00:14:22 -0500

    code for deployment

--- app/config.py
+++ app/config.py
@@ -2,15 +2,16 @@ import os
 from dotenv import load_dotenv
 
 # Load environment variables from the `.env` file in the root directory
-dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
-load_dotenv(dotenv_path)
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///local.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")  # No fallback for production
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "default-jwt-secret-key")
-    BASE_URL = 'http://127.0.0.1:5000'  # Base URL for serving uploaded files
+
+    BASE_URL = 'http://127.0.0.1:5000'  # Used for serving local files
+
     UPLOAD_FOLDER_COLLABORATIONS = os.getenv(
         "UPLOAD_FOLDER_COLLABORATIONS",
         os.path.join(os.getcwd(), 'uploads/collaborations'),
