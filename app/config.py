754c75e04260a220bbc871a1cc2c10134d39612e
anh <ngducanh126@gmail.com>
2024-12-31 22:38:55 -0500

    config.py for deployment

--- app/config.py
+++ app/config.py
@@ -1,10 +1,15 @@
-import os 
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from the `.env` file in the root directory
+dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
+load_dotenv(dotenv_path)
 
 class Config:
-    SECRET_KEY = 'your-secret-key'
+    SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///local.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'your-jwt-secret-key'
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "default-jwt-secret-key")
     BASE_URL = 'http://127.0.0.1:5000'  # Base URL for serving uploaded files
     UPLOAD_FOLDER_COLLABORATIONS = os.getenv(
         "UPLOAD_FOLDER_COLLABORATIONS",
@@ -14,5 +19,4 @@ class Config:
         "UPLOAD_FOLDER_PROFILE",
         os.path.join(os.getcwd(), 'uploads/users'),
     )
-
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
